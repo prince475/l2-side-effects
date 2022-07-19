9913eb882eee6bca7c135834b9b59e3fbf8ddcaf
Ian Okumu <okumu.otsembo@gmail.com>
2022-07-19 05:29:52 +0300

    Initial Setup

---
+++ src/components/user/User.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const RANDOM_USER_API = "https://randomuser.me/api"
+
+function User(props){
+
+    const [name, setName] = useState("sample name")
+    const [email, setEmail] = useState("sample email")
+    const [count, setCount] = useState(0)
+
+    function updateCount(currentCount){
+        setCount(currentCount + 1)
+    }
+
+
+    return(
+        <div>
+            <center>
+                <h2>NAME: {name}</h2>
+                <h5>EMAIL: {email}</h5>
+                <button onClick={() => updateCount(count)}>RANDOMIZE</button>
+            </center>
+        </div>
+    )
+
+}
+
+export default User
