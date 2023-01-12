e0577f3e5204a9542f75c2501bf94613e4299f21
prince475 <danielmwai475@gmail.com>
2023-01-12 12:27:46 +0300

    Adding useEffect tou our code

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -4,13 +4,49 @@ const RANDOM_USER_API = "https://randomuser.me/api"
 
 function User(props){
 
+    //set states
     const [name, setName] = useState("sample name")
     const [email, setEmail] = useState("sample email")
     const [count, setCount] = useState(0)
 
+    //function to implement count increase
     function updateCount(currentCount){
         setCount(currentCount + 1)
     }
+
+    //useEffect code
+
+        function logger(){
+        fetch(RANDOM_USER_API)
+            .then((response)=>response .json())
+            .then((data) => {
+                const results = data.results
+                const user = results[0]
+                const userName = `${user.name.title} ${user.name.first} ${user.name.last}`
+                const userEmail = user.email
+
+                setName(userName)
+                setEmail(userEmail)
+            })
+            
+    }
+      
+        useEffect(logger, [count])
+    //you can add other values that you need to randomise
+
+    // useEffect(() => {
+    //     fetch(RANDOM_USER_API)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const results = data.results
+    //             const user = results[0]
+    //             const userName = `${user.name.title} ${user.name.first} ${user.name.last}`
+    //             const userEmail = user.email
+
+    //             setName(userName)
+    //             setEmail(userEmail)
+    //         })
+    // },[count])
 
 
     return(
